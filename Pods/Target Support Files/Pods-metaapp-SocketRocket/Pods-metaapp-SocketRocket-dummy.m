464f7754b049a5c88caafa5dc53ca78019b422bf
MM <goblortikus-github@yahoo.com>
2016-01-07 18:55:44 -0800

    clean new

--- Pods/Target Support Files/Pods-metaapp-SocketRocket/Pods-metaapp-SocketRocket-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_metaapp_SocketRocket : NSObject
-@end
-@implementation PodsDummy_Pods_metaapp_SocketRocket
-@end
--- Pods/Target Support Files/Pods-metaapp-libPusher/Pods-metaapp-libPusher-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_metaapp_libPusher : NSObject
-@end
-@implementation PodsDummy_Pods_metaapp_libPusher
-@end
